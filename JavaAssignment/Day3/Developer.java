6ef91a7e51d188a3d969debe4faf1bd7b6a27612
akarsh4 <76525832+akarsh4@users.noreply.github.com>
2022-09-11 17:05:06 +0530

    Update Developer.java

--- JavaAssignment/Day3/Developer.java
+++ JavaAssignment/Day3/Developer.java
@@ -5,7 +5,7 @@ public class Developer extends Employee{
 	@Override
 	public void salCal() {
 		// TODO Auto-generated method stub
-		System.out.println("Developer salary "+this.empSal);
+		System.out.println("Developer yearly salary is  "+this.empSal*12);
 		
 	}
 
