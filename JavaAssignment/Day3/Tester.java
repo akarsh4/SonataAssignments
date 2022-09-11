1517ae6a3ebe5340863ab1c23c5d8ba4cfb963f3
akarsh4 <76525832+akarsh4@users.noreply.github.com>
2022-09-11 17:05:35 +0530

    Update Tester.java

--- JavaAssignment/Day3/Tester.java
+++ JavaAssignment/Day3/Tester.java
@@ -5,7 +5,7 @@ public class Tester extends Employee{
 	@Override
 	public void salCal() {
 		// TODO Auto-generated method stub
-		System.out.println("Tester salary "+this.empSal);		
+		System.out.println("Tester yearly salary is "+this.empSal*12);		
 	}
 
 }
