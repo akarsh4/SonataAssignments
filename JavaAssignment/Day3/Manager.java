c183a74f4cf832f304b9780b7d4ca281f1698c01
akarsh4 <76525832+akarsh4@users.noreply.github.com>
2022-09-11 17:04:16 +0530

    Update Manager.java

--- JavaAssignment/Day3/Manager.java
+++ JavaAssignment/Day3/Manager.java
@@ -6,7 +6,7 @@ public class Manager extends Employee{
 	@Override
 	public void salCal() {
 		// TODO Auto-generated method stub
-	System.out.println("Manager salary "+this.empSal);	
+	System.out.println("Manager yearly salary is "+this.empSal*12);	
 	}
 
 }
